e0e87c067e947186aec35f1cfb5b8e349a01ac0a
Felipe Lavín Z <felipe@bloom-ux.com>
2020-02-13 13:30:10 -0300

    turbolinks

--- inc/class-personal-site-template.php
+++ inc/class-personal-site-template.php
@@ -142,9 +142,10 @@ class Personal_Site_Template {
 	 */
 	public function do_wp_enqueue_scripts() {
 		wp_enqueue_style( 'personal-site-theme', get_stylesheet_directory_uri() . '/css/app.css', array(), '1.0.0', 'screen' );
-		wp_enqueue_script( 'what-input', get_stylesheet_directory_uri() . '/node_modules/what-input/dist/what-input.js', array(), '5.2.6', true );
-		wp_enqueue_script( 'foundation', get_stylesheet_directory_uri() . '/node_modules/foundation-sites/dist/js/foundation.js', array( 'jquery-core', 'what-input' ), '6.5.3', true );
-		wp_enqueue_script( 'personal-site-js', get_stylesheet_directory_uri() . '/js/app.js', array( 'foundation' ), '1.0.0', true );
+		wp_enqueue_script( 'turbolinks', get_stylesheet_directory_uri() .'/node_modules/turbolinks/dist/turbolinks.js', array(), '5.2.0', false );
+		wp_enqueue_script( 'what-input', get_stylesheet_directory_uri() . '/node_modules/what-input/dist/what-input.js', array(), '5.2.6', false );
+		wp_enqueue_script( 'foundation', get_stylesheet_directory_uri() . '/node_modules/foundation-sites/dist/js/foundation.js', array( 'jquery-core', 'what-input' ), '6.5.3', false );
+		wp_enqueue_script( 'personal-site-js', get_stylesheet_directory_uri() . '/js/app.js', array( 'foundation' ), '1.0.0', false );
 	}
 
 }
